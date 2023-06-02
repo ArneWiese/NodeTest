5dc08aba3d9d739b98374b5fe4c694df22bdfff0
Arne Wiese <arnewiese@MacBook-Pro-von-Arne.local>
2023-06-02 23:35:35 +0200

    Added new node project

---
+++ src/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'some_database'
+})
+
+async function getUser(username) {
+  const [rows] = await connection.promise().query(
+    `SELECT * 
+      FROM users 
+      WHERE username = ?`,
+    [username]
+  )
+
+  return rows[0]
+}
+
+async function createUser(username, password) {
+  const { insertId } = await connection.promise().query(
+    `INSERT INTO users (username, password) 
+      VALUES (?, ?)`,
+    [username, password]
+  )
+
+  return insertId
+}
+
+module.exports = {
+    getUser,
+    createUser
+}
